0b364e81c5c49575e8e2bd275704db198a5454f0
James Hulley <james.hulley@gmail.com>
2018-11-07 22:15:20 -0800

    test tweaks

--- examples/ensemble/fit_double_sample_forest.py
+++ examples/ensemble/fit_double_sample_forest.py
@@ -13,12 +13,9 @@ from sklearn.ensemble import DoubleSampleForest
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
-# Dimensions of covariates
-d = 6
-# Number of observations
-n = 5000
-
-X = np.random.uniform(0, 1, size=(n, d))
+num_features = 6
+num_observations = 5000
+X = np.random.uniform(0, 1, size=(num_observations, num_features))
 y = []
 w = []
 
@@ -39,12 +36,11 @@ tau = np.multiply(func_x(X[:,0]),func_x(X[:,1]))
 X_train, X_test, y_train, y_test, w_train, w_test, tau_train, tau_test = train_test_split(
     X, y, w, tau, test_size=0.5, random_state=4)
 
-regr = DoubleSampleForest(n_estimators=100, max_depth=30,
-                             random_state=2)
+regr = DoubleSampleForest(n_estimators=100, max_depth=30, random_state=2)
 regr.fit(X_train, y_train, w_train)
 
 y_pred = regr.predict_outcomes(X_test)
-tau_hat = np.multiply(y_pred[:,0], y_pred[:,1])
-score = mean_squared_error(tau_test, tau_hat)
+tau_pred = np.multiply(y_pred[:, 0], y_pred[:,1])
+score = mean_squared_error(tau_test, tau_pred)
 
 print(score)
